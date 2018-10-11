987af72c03e010a87892a4dcea1d3e8aa43e4ccb
Andrew Motley <andrew.motley.2021@student.lhschools.org>
2018-10-11 08:06:51 -0400

    finished project in IntelliJ

--- src/RockPapersScissors.java
+++ src/RockPapersScissors.java
@@ -14,6 +14,9 @@ public class RockPapersScissors {
         String computerChoice;
         int number;
         String again;
+        int senseiScore,samuraiScore;
+        samuraiScore = 0;
+        senseiScore = 0;
 
         keyboard = new Scanner(System.in);
         do {
@@ -35,36 +38,61 @@ public class RockPapersScissors {
                 if(weaponpicked.equals("rock")){
                     if(computerChoice.equals("paper")){
                         System.out.println("Paper beats Rock: Sensei wins");
+                        senseiScore +=1;
+                        System.out.println("The sensei's score is "+senseiScore);
 
                     }
                     else{
                         System.out.println("Rock beats Scissors: Samurai Wins");
+                        samuraiScore +=1;
+                        System.out.println("Your score is "+samuraiScore);
                     }
                 }
                 if(weaponpicked.equals("paper")){
                     if(computerChoice.equals("scissors")){
                         System.out.println("Scissors beat Paper: Sensei wins");
+                        senseiScore +=1;
+                        System.out.println("The sensei's score is "+senseiScore);
 
                     }
                     else{
                         System.out.println("Paper beats Rock: Samurai Wins");
+                        samuraiScore +=1;
+                        System.out.println("Your score is "+samuraiScore);
                     }
                 }
                 if(weaponpicked.equals("scissors")){
                     if(computerChoice.equals("rock")){
                         System.out.println("Rock beats Scissors: Sensei wins");
+                        senseiScore +=1;
+                        System.out.println("The sensei's score is "+senseiScore);
 
                     }
                     else{
-                        System.out.println("Scissors beat Rock: Samurai Wins");
+                        System.out.println("Scissors beat Paper: Samurai Wins");
+                        samuraiScore +=1;
+                        System.out.println("Your score is "+samuraiScore);
                     }
                 }
 
             }System.out.println("Would you like to challenge me again? (yes or no)");
-            again = keyboard.next().toLowerCase();
+            again = keyboard.nextLine().toLowerCase();
         }while(again.equals("yes"));
-        System.out.println("if you lose today, win tomorrow. In this never-ending spirit of challenge is found in the" +
-                " heart of a victor");
+
+        System.out.println("The Sensei's final score is "+senseiScore);
+        System.out.println("Your final score is "+samuraiScore);
+
+        if(samuraiScore==senseiScore){
+            System.out.println("You fought a tough battle");
+        }
+        else if (samuraiScore>senseiScore){
+            System.out.println("strong you are");
+        }
+        else if (samuraiScore<senseiScore){
+            System.out.println("You were a waste of time anyway");
+        }
+        System.out.println("come back if you dare");
     }
+
 
 }
